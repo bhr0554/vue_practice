d2783f9f94b6bce1358efb8857ce3eca36480d5f
bhr0554 <bhr5004@gmail.com>
2021-02-18 18:13:14 +0900

    Mysql DB 연결

--- webpack_backend/app.js
+++ webpack_backend/app.js
@@ -38,4 +38,26 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+/** DB연결 */
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 'data~secret!',
+  database: 'study_db'
+});
+
+connection.connect(function (err) {
+  if(err) {
+    console.error('mysql connection error');
+    console.error(err);
+    throw err;
+  } else {
+    console.log('mysql connection success');
+  }
+});
+/** //DB연결 */
+
 module.exports = app;
